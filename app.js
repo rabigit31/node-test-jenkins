1cd2a3e2bc43d62e15651c44426251183af4adde
rabi1digiunity <rabi@digiunity.com>
2023-11-14 19:04:03 +0530

    api updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3020
 
 app.get('/', (req, res) => {
-    res.send('Hello World!ttt hi rabi bhakat Bajran Bali 6666')
+    res.send('Hello World!ttt hi rabi bhakat Bajran Bali 6666 66')
 })
 
 app.listen(port, () => {
