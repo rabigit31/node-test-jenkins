94f1cfe132449bf17276832bd6436eb314584f91
rabi1digiunity <rabi@digiunity.com>
2023-11-16 14:00:48 +0530

    api updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3020
 
 app.get('/', (req, res) => {
-    res.send('This tis my first docer dfggdfhgfdhg jdhjhdfjh 888888 fjjhhf 55555 uyuyuy Test process ffgjhgjfhg fjgjfggh')
+    res.send('Ok need to be test')
 })
 
 app.listen(port, () => {
